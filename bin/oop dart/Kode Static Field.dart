f5e82ae92ee177f2940d3b71823f3099269f86e9
riza <mrezaanwar63@gmail.com>
2024-10-23 19:30:05 +0700

    Static Field

---
+++ bin/oop dart/Kode Static Field.dart
@@ -0,0 +1,9 @@
+class Application {
+  static final String author = "Muhammad Reza Anwar"; // Corrected field name
+  static final String name = "Belajar Pemrograman Dart";
+}
+
+void main() {
+  print(Application.name);
+  print(Application.author); // Use the corrected field name
+}
