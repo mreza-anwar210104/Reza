f50fed0ca011565ad46955a7b6735b02b26571d9
riza <mrezaanwar63@gmail.com>
2024-10-23 21:07:48 +0700

    Boolean

---
+++ bin/Kode Boolean reza2.dart
@@ -0,0 +1,7 @@
+void main() {
+  bool finish = false; // Deklarasi boolean dengan nilai awal false
+  print('Apakah selesai? $finish');  // Output: Apakah selesai? false
+
+  finish = true;  // Mengubah nilai boolean menjadi true
+  print('Apakah selesai? $finish');  // Output: Apakah selesai? true
+}
