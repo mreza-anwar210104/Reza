942bc6d64a2a6ac7269651292217555ade2f4812
riza <mrezaanwar63@gmail.com>
2024-11-06 10:08:23 +0700

    Bukan Generic

---
+++ bin/oop dart/Dart Generic/Kode Bukan Generic.dart
@@ -0,0 +1,9 @@
+class Data {
+  dynamic data;
+}
+
+void main(List<String> argument) {
+  var data = Data();
+  data.data = "Muhammad Reza Anwar";
+  print(data.data);
+}                                                    
